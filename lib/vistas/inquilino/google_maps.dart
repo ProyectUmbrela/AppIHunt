2fad6411d1e02372d29a24e53a5ca3c1191152ff
desonses <desonses@gmail.com>
2021-10-30 21:08:50 -0500

    changes ok

--- lib/vistas/inquilino/google_maps.dart
+++ lib/vistas/inquilino/google_maps.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:ihunt/vistas/userView.dart';
+import 'package:ihunt/vistas/inquilino/userView.dart';
 import 'dart:async';
 
 // slider images
