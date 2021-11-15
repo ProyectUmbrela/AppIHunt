02f785fd4cc4dbd3abb2dd04f94386836e30c18b
desonses <desonses@gmail.com>
2021-11-15 13:44:05 -0600

    notificaciones ok

---
+++ lib/vistas/inquilino/notificationes_inquilino.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NotificationesInquilino extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return NotificationesInquilinoState();
+  }
+  
+}
+class NotificationesInquilinoState extends State<NotificationesInquilino>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+  
+}
