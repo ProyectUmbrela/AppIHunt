3352f6550825fee6616e135bb38c3a7dfaf21355
desonses <desonses@gmail.com>
2021-11-18 21:58:27 -0600

    detalles habitacion

---
+++ lib/vistas/inquilino/loginTest.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginTest extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginTestState();
+  }
+  
+}
+class LoginTestState extends State<LoginTest>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+  
+}
