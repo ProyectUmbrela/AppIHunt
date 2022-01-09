e07b8468aa19a73d103fff1695c2ce09930804f0
desonses <desonses@gmail.com>
2022-01-09 00:51:21 -0600

    invitaciones

---
+++ lib/vistas/inquilino/detalles_invitacion.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetallesInvitacion extends StatefulWidget {
+  const DetallesInvitacion({Key key}) : super(key: key);
+
+  @override
+  _DetallesInvitacionState createState() => _DetallesInvitacionState();
+}
+
+class _DetallesInvitacionState extends State<DetallesInvitacion> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
