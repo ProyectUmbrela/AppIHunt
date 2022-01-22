a6591a8ee96f28a9a1156cdac3c2a571f03f93c0
desonses <desonses@gmail.com>
2022-01-22 17:17:06 -0600

    ok

---
+++ lib/vistas/inquilino/inicio.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Inicio extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return InicioState();
+  }
+  
+}
+class InicioState extends State<Inicio>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+  
+}
